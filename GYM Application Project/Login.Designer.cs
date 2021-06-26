03e8ef9e529ec242fbd080f94ab1a0ee34fd9a13
mandakini76 <69083317+mandakini76@users.noreply.github.com>
2021-06-26 20:19:06 +0530

    login updated

--- GYM Application Project/Login.Designer.cs
+++ GYM Application Project/Login.Designer.cs
@@ -32,8 +32,17 @@ namespace GYM_Application_Project
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.Tpass = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Tuser = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -41,44 +50,129 @@ namespace GYM_Application_Project
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(570, 703);
+            this.panel1.Size = new System.Drawing.Size(428, 571);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.Tpass);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.Tuser);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(570, 0);
+            this.panel2.Location = new System.Drawing.Point(428, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(612, 703);
+            this.panel2.Size = new System.Drawing.Size(458, 571);
             this.panel2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Location = new System.Drawing.Point(84, 325);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 26);
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(84, 213);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 26);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(84, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 34);
+            this.button1.TabIndex = 14;
+            this.button1.Text = " Login";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(84, 353);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(299, 1);
+            this.panel4.TabIndex = 13;
+            // 
+            // Tpass
+            // 
+            this.Tpass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(45)))), ((int)(((byte)(57)))));
+            this.Tpass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Tpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tpass.ForeColor = System.Drawing.Color.White;
+            this.Tpass.Location = new System.Drawing.Point(114, 332);
+            this.Tpass.Name = "Tpass";
+            this.Tpass.Size = new System.Drawing.Size(269, 19);
+            this.Tpass.TabIndex = 12;
+            this.Tpass.Text = "Password";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(84, 241);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(299, 1);
+            this.panel3.TabIndex = 11;
+            // 
+            // Tuser
+            // 
+            this.Tuser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(45)))), ((int)(((byte)(57)))));
+            this.Tuser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Tuser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tuser.ForeColor = System.Drawing.Color.White;
+            this.Tuser.Location = new System.Drawing.Point(114, 220);
+            this.Tuser.Name = "Tuser";
+            this.Tuser.Size = new System.Drawing.Size(269, 19);
+            this.Tuser.TabIndex = 10;
+            this.Tuser.Text = "Username";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(185, 237);
+            this.label1.Location = new System.Drawing.Point(76, 101);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(243, 55);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Mandakini";
+            this.label1.Size = new System.Drawing.Size(291, 44);
+            this.label1.TabIndex = 9;
+            this.label1.Text = " Welcome back!";
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(45)))), ((int)(((byte)(57)))));
-            this.ClientSize = new System.Drawing.Size(1182, 703);
+            this.ClientSize = new System.Drawing.Size(886, 571);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -87,6 +181,13 @@ namespace GYM_Application_Project
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox Tpass;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox Tuser;
         private System.Windows.Forms.Label label1;
     }
 }
--- GYM Application Project/Login.cs
+++ GYM Application Project/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace GYM_Application_Project
 {
@@ -15,6 +16,79 @@ namespace GYM_Application_Project
         public Login()
         {
             InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection(""); // Add connection string here
+
+        private void Tuser_Enter(object sender, EventArgs e)
+        {
+            Tuser.Text = "";
+        }
+
+        private void Tuser_MouseEnter(object sender, EventArgs e)
+        {
+            Tuser.Text = "";
+        }
+
+        private void Tpass_MouseEnter(object sender, EventArgs e)
+        {
+            Tpass.Text = "";
+            Tpass.PasswordChar = '*';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Tuser.Text == "" || Tpass.Text == "")
+            {
+                showErrorMsg();
+            }
+            else
+            {
+                login();
+            }
+        }
+
+        private void showErrorMsg()
+        {
+            MessageBox.Show("Login Faild. Try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void login()
+        {
+            try
+            {
+                connection.Open();
+                String query = "select id from users where username = '" + Tuser.Text.Trim() + "' and password = '" + Tpass.Text.Trim() + "'";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    showErrorMsg();
+                }
+                else
+                {
+                    Form1 dashboard = new Form1();
+                    dashboard.Show();
+                    this.Hide();
+                }
+                connection.Close();
+            }
+
+            catch
+            {
+                MessageBox.Show("Something went wrong", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Tpass_Enter(object sender, EventArgs e)
+        {
+            Tpass.Text = "";
+            Tpass.PasswordChar = '*';
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
